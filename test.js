6b33f210335a0d9f9580352bdddd2f5d1d5856c2
Azzam Hamid <azzam@JokersCircus.local>
2022-09-26 20:43:31 -0400

    week1day1

---
+++ test.js
@@ -0,0 +1,67 @@
+console.log("Hello World!")
+console.log(2)
+console.log(2+2)
+console.log(10/3)
+console.log(2*2)
+console.log(9-3)
+console.log(10%3)
+console.log(10%2)
+console.log(9%2)
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+console.log(true && true)
+console.log(false && false)
+console.log(true && false)
+console.log(false && true)
+console.log(!true)
+console.log(!!true)
+console.log(!false)
+console.log(!!false)
+console.log(10 > 10)
+console.log(10 < 20)
+console.log(25 >= 5)
+console.log(25 <= 25)
+console.log(12 === 11)
+console.log(12 !== 11)
+console.log(12 == "12")
+let str="hello"
+console.log(str[1])
+console.log("hello"[1])
+console.log("hello".length)
+console.log("hello".indexOf('h'))
+console.log("hello".indexOf('l'))
+console.log("hello".indexOf('x'))
+console.log("race".concat("car"))
+console.log("race" + "car")
+console.log("hello" + " " + "world")
+console.log(4 + "hi")
+console.log(4 * "hi")
+console.log(4 * '10')
+console.log("hello".toUpperCase());
+console.log("HELLO".toLowerCase());
+console.log("hello".slice());
+console.log("hello".slice(1));
+console.log("hello".slice(1,3)); // 1 inclusive 3 exclusive
+let name = "Jonathan";
+console.log(name);
+console.log(name === "Jonathan")
+console.log(name="Kevin")
+console.log(name)
+let firstName ="Azzam"
+let lastName ="Hamid"
+let number ="amazing" //bad naming
+//Don't use reserved keywords like let null =, let NaN, let 2star, let(str) Bad naming
+let test
+console.log(test)
+//Conditionals are mutually exclusive
+if(20 === 20){console.log("they are equal");} else {console.log("not equal");}
+let num =20;
+if(num === 20.5) {
+    console.log("equals 20"); 
+} else if (num > 20) {
+    console.log("greater"); 
+} else{ 
+    console.log("less than");
+}
