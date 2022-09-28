aa1d505f8d68cabaebb4da19bc24bc43bfba77e9
Azzam Hamid <azzamhamid@gmail.com>
2022-09-28 19:26:55 -0400

    day3Lecture

--- Week1/Day3/testDay3.js
+++ Week1/Day3/testDay3.js
@@ -1,14 +1,14 @@
 //Console log 1-10
-console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
-console.log(5)
-console.log(6)
-console.log(7)
-console.log(8)
-console.log(9)
-console.log(10)
+//console.log(1)
+//console.log(2)
+//console.log(3)
+//console.log(4)
+//console.log(5)
+//console.log(6)
+//console.log(7)
+//console.log(8)
+//console.log(9)
+//console.log(10)
 
 function count(start, end) {
     for (let i = start; i <= end; i += 1){
@@ -19,3 +19,13 @@ function count(start, end) {
 count(1, 10)
 // i ++ is the same thing is i +=1
 // David showed me a shortcut Command + D highlight ALL COUNT changed to count
+//THe high level languages are written in low level languages, the reason we have highlevel languages so that we can work faster.]high level= ruby, python,javascript low level languages c# c+++, java
+console.log("while loop")
+function printRange (start, end){
+    let i = start;
+    while (i <= end){
+        console.log(i);
+        i++;
+    }
+}
+printRange(7,21)
